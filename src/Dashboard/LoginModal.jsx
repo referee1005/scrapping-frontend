b8ce3df63da58d029ef4b476cee6b63d6aaea602
Mohit <mapinuly88@gmail.com>
2024-07-11 13:34:42 +0530

    fixed the extra space issue

--- src/Dashboard/LoginModal.jsx
+++ src/Dashboard/LoginModal.jsx
@@ -40,49 +40,49 @@ export const LoginModal = ({ open, handleClose }) => {
         setForgotPassModal(false)
     }
   return (
-    <Toolbar>
-            <Dialog open={open} onClose={handleClose} className="multilang_dialog login-Modal">
-              <Toolbar style={{display:'flex',justifyContent:'space-between'}} className="header_top">
-                <DialogTitle>Add project to your management panel</DialogTitle>
-                <button onClick={handleClose}>
-                <svg width="18" height="18" fill="none" viewBox="0 0 24 24"><g clip-path="url(#a)"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M7 7l10 10M7 17L17 7"></path></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h24v24H0z"></path></clipPath></defs></svg>
-                </button>
-              </Toolbar>
-              <Toolbar class="login-modal">
-                <Toolbar class="login-container-right">
-                    <h2>Sign up now to:</h2>
-                    <ul class="">
-                        <li>Manage your TMs, glossaries and MT engines</li>
-                        <li>Access the management panel</li>
-                        <li>Translate Google Drive files</li>
-                    </ul>
-                    <a class="register-button btn-confirm-medium">Sign up</a>
-                </Toolbar>
-                <Toolbar class="login-container-left">
-                    <a class="google-login-button btn-confirm-medium"></a>
-                    <Toolbar class="login-form-container">
-                        <Toolbar class="form-divider">
-                            <Toolbar class="divider-line"></Toolbar>
-                            <span>OR</span>
-                            <Toolbar class="divider-line"></Toolbar>
-                        </Toolbar>
-                        <Toolbar>
-                            <input type="text" placeholder="Email" name="emailAddress" tabindex="1" value=""/>
-                        </Toolbar>
-                        <Toolbar>
-                            <input type="password" placeholder="Password" name="password" tabindex="2" value=""/>
-                        </Toolbar>
-                        <a class="login-button btn-confirm-medium sing-in disabled" tabindex="3">
-                            <span class="button-loader "></span> Sign in 
-                        </a>
-                        <br/>
-                        <span class="forgot-password" onClick={handleForgotPassModal}>Forgot password?</span>
+    <>
+        <Dialog open={open} onClose={handleClose} className="multilang_dialog login-Modal">
+            <Toolbar style={{display:'flex',justifyContent:'space-between'}} className="header_top">
+            <DialogTitle>Add project to your management panel</DialogTitle>
+            <button onClick={handleClose}>
+            <svg width="18" height="18" fill="none" viewBox="0 0 24 24"><g clip-path="url(#a)"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M7 7l10 10M7 17L17 7"></path></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h24v24H0z"></path></clipPath></defs></svg>
+            </button>
+            </Toolbar>
+            <Toolbar class="login-modal">
+            <Toolbar class="login-container-right">
+                <h2>Sign up now to:</h2>
+                <ul class="">
+                    <li>Manage your TMs, glossaries and MT engines</li>
+                    <li>Access the management panel</li>
+                    <li>Translate Google Drive files</li>
+                </ul>
+                <a class="register-button btn-confirm-medium">Sign up</a>
+            </Toolbar>
+            <Toolbar class="login-container-left">
+                <a class="google-login-button btn-confirm-medium"></a>
+                <Toolbar class="login-form-container">
+                    <Toolbar class="form-divider">
+                        <Toolbar class="divider-line"></Toolbar>
+                        <span>OR</span>
+                        <Toolbar class="divider-line"></Toolbar>
                     </Toolbar>
+                    <Toolbar>
+                        <input type="text" placeholder="Email" name="emailAddress" tabindex="1" value=""/>
+                    </Toolbar>
+                    <Toolbar>
+                        <input type="password" placeholder="Password" name="password" tabindex="2" value=""/>
+                    </Toolbar>
+                    <a class="login-button btn-confirm-medium sing-in disabled" tabindex="3">
+                        <span class="button-loader "></span> Sign in 
+                    </a>
+                    <br/>
+                    <span class="forgot-password" onClick={handleForgotPassModal}>Forgot password?</span>
                 </Toolbar>
             </Toolbar>
-          
-            </Dialog>
-            <ForgotPassModal open={forgotPassModal} handleClose={closeForgotModal}/>
-          </Toolbar>
+        </Toolbar>
+        
+        </Dialog>
+        <ForgotPassModal open={forgotPassModal} handleClose={closeForgotModal}/>
+    </>
   )
 }
--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,51 +1,161 @@
 import React, { useState } from 'react';
 import { IconButton, Paper, Box, Typography } from '@mui/material';
-import ChatIcon from '@mui/icons-material/Chat';
-import CloseIcon from '@mui/icons-material/Close';
-import { ChatWindow } from './ChatWindow';
-import { ChatInput } from './ChatInput';
+import { TextField, InputAdornment, Toolbar,Autocomplete,List, ListItem, ListItemText, Divider  } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import RobotIcon from '@mui/icons-material/Android';
 
-const ChatBot = () => {
-  const [open, setOpen] = useState(false);
-  const [messages, setMessages] = useState([]);
+const ChatWidget = () => {
+    const [open, setOpen] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [activeChatSection, setActiveChatSection] = useState(false)
+    const [input, setInput] = useState('');
+    const options = ['Article 1', 'Article 2', 'Article 3'];
 
-  const handleToggle = () => {
-    setOpen(!open);
-  };
+    const handleToggle = () => {
+        setOpen(!open);
+    };
 
-  const handleSend = (message) => {
-    setMessages([...messages, { text: message, sender: 'user' }]);
-    // Simulate bot response
-    setTimeout(() => {
-      setMessages(prevMessages => [...prevMessages, { text: `Bot response to: ${message}`, sender: 'bot' }]);
-    }, 1000);
-  };
+    const handleSend = () => {
+        if (input.trim()) {
+            setMessages([...messages, { text: input, sender: 'user' }]);
+            setInput('');
+            setTimeout(() => {
+                setMessages((prevMessages) => [
+                    ...prevMessages,
+                    { text: 'This is a response from the chatbot.', sender: 'bot' },
+                ]);
+            }, 1000);
+        }
+    };
+    const handleChatSection = () => {
+        setActiveChatSection(true)
+    }
 
-  return (
-    <Box position="fixed" bottom={16} right={16} zIndex={1000}>
-      {open && (
-        <Paper elevation={3} style={{ width: '300px', height: '400px', display: 'flex', flexDirection: 'column' }}>
-          <Box display="flex" justifyContent="space-between" alignItems="center" padding={2} bgcolor="primary.main" color="white">
-            <Typography variant="h6">Chatbot</Typography>
-            <IconButton color="inherit" onClick={handleToggle}>
-              <CloseIcon />
+    const handleBack = () => {
+        setActiveChatSection(false);
+    };
+    return (
+        <Box position="fixed" bottom={16} right={16} zIndex={1000} display="flex" flexDirection="column" alignItems="end">
+            {open && (
+                <Paper elevation={3} style={{ width: '350px', height: '550px', display: 'flex', flexDirection: 'column', marginBottom: '16px', backgroundColor: '#f5f8fa' }}>
+                    <Box display="flex" justifyContent="space-between" alignItems="center" padding={2} bgcolor="#002b5c" color="white">
+                    {activeChatSection && (
+                                <IconButton onClick={handleBack} style={{ color: 'white' }}>
+                                    {/* <ArrowBackIcon /> */}
+                                    <img src="./img/back.svg" alt="" />
+                                </IconButton>
+                            )}
+                    <Typography variant="h6">{activeChatSection ? 'Ilaria, Ambra, Valentina' : 'Help'}</Typography>
+                    </Box>
+
+                    {/* Chat input section starts */}
+
+                   {!activeChatSection&& ( <Toolbar style={{ display: 'inline-block' }}>
+                        <div className='bot-first' style={{ display: 'inline-block', backgroundColor: '#fff', marginTop: '10px', minWidth: '307px', marginBottom: '20px' }}>
+                            <div style={{ display: 'flex', justifyContent: 'start' }}>
+                                <p style={{ marginTop: '4px' }}>Find Answers Quickly</p>
+                            </div>
+                            <Autocomplete
+                                    freeSolo
+                                    options={options}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            placeholder='Search Articles'
+                                            variant="outlined"
+                                            fullWidth
+                                            value={input}
+                                            onChange={(e) => setInput(e.target.value)}
+                                            onKeyPress={(e) => {
+                                                if (e.key === 'Enter') handleSend();
+                                            }}
+                                            InputProps={{
+                                                ...params.InputProps,
+                                                startAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <SearchIcon />
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    )}
+                                />
+
+                        </div>
+
+                        <Toolbar className='Chat-section'>
+
+                            <p>Your Chats</p>
+                            <img onClick={handleChatSection} style={{ cursor: 'pointer' }} src="./img/edit.svg" alt="" />
+
+                        </Toolbar>
+                        <Toolbar className='chats'>
+
+
+                        </Toolbar>
+                    </Toolbar>
+                   )}
+
+                    {/* Chat input section ends  */}
+
+
+                    {/* Chat section starts */}
+                    {activeChatSection && (
+                        <Box display="flex" flexDirection="column" flexGrow={1} justifyContent="space-between">
+                                <List style={{ overflowY: 'auto', flexGrow: 1 }}>
+                                {messages.map((message, index) => (
+                                    <ListItem key={index} alignItems="flex-start">
+                                        {message.sender === 'user' ? (
+                                            <>
+                                                <PersonIcon style={{ marginRight: '8px', alignSelf: 'flex-start', color: '#2196f3' }} />
+                                                <ListItemText
+                                                    primary={message.text}
+                                                    style={{ textAlign: 'left' }}
+                                                />
+                                            </>
+                                        ) : (
+                                            <>
+                                                <RobotIcon style={{ marginLeft: '8px', alignSelf: 'flex-start', color: '#f44336' }} />
+                                                <ListItemText
+                                                    primary={message.text}
+                                                    style={{ textAlign: 'left' }}
+                                                />
+                                            </>
+                                        )}
+                                    </ListItem>
+                                ))}
+                            </List>
+                            <Divider />
+                            <Box display="flex" padding={2}>
+                                <TextField
+                                    variant="outlined"
+                                    fullWidth
+                                    placeholder="Write a message"
+                                    value={input}
+                                    onChange={(e) => setInput(e.target.value)}
+                                    onKeyPress={(e) => {
+                                        if (e.key === 'Enter') handleSend();
+                                    }}
+                                />
+                                <IconButton color="primary" onClick={handleSend}>
+                                    <img src="./img/send.svg" alt="" />
+                                </IconButton>
+                            </Box>
+                        </Box>
+                    )}
+                    {/* Chat section ends */}
+
+
+                    <Box padding={2}>
+                    </Box>
+                </Paper>
+            )}
+            <IconButton color="primary" onClick={handleToggle} style={{ backgroundColor: 'rgb(0, 43, 92)', marginBottom: '30px' }} className='chatbot_btn'>
+                {open ? <img src='./img/chatClose.svg' /> : <img src='./img/message.svg' />}
             </IconButton>
-          </Box>
-          <Box flex={1} padding={2} overflow="auto">
-            <ChatWindow messages={messages} />
-          </Box>
-          <Box padding={2}>
-            <ChatInput onSend={handleSend} />
-          </Box>
-        </Paper>
-      )}
-      {!open && (
-        <IconButton color="primary" onClick={handleToggle} style={{ backgroundColor: 'white' }}>
-          <ChatIcon />
-        </IconButton>
-      )}
-    </Box>
-  );
+        </Box>
+    );
 };
 
-export default ChatBot;
+export default ChatWidget;
